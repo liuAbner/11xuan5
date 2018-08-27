d3f6d8231187860c4ffa40c443f03bbf0f543759
abnerljx <liujiexiong10@sina.com>
2018-08-27 10:51:37 +0800

    第一次提交：初始化仓库，并提交代码

---
+++ 0.0.1/src/test/java/com/example/administrator/a11xuan5/0/0/1/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package com.example.administrator.a11xuan5
+
+.0.0.1
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
